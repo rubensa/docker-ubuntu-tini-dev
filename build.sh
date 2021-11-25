0cd535b7f079c732cfabf97f02e38658de4117e7
Ruben Suarez <rubensa@gmail.com>
2021-11-25 17:23:20 +0100

    Set image tag as 18.04
    Remove code specifig for Ubuntu 22.04
    For Ubuntu libgdbm version available is 5 (not 6)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DOCKER_REPOSITORY_NAME="rubensa"
 DOCKER_IMAGE_NAME="ubuntu-tini-dev"
-DOCKER_IMAGE_TAG="latest"
+DOCKER_IMAGE_TAG="18.04"
 
 # see: https://github.com/docker/buildx/issues/495#issuecomment-761562905
 #docker buildx build --platform=linux/amd64,linux/arm64 --no-cache --progress=plain --pull \
